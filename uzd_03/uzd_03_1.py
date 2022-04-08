003dbdbb38b7a11ca91a74fa9cb5b8d99cd85f29
ViPelc <vkozus@gmail.com>
2022-04-08 20:04:41 +0300

    8. aprila_2dalas_uzd_izpilde

--- uzd_03/uzd_03_1.py
+++ uzd_03/uzd_03_1.py
@@ -1,2 +1,13 @@
 # Apgriezt pozitīva skaitļa ciparu secību. Piemērs: 12345 => 54321
 
+num=input("Ievadi pozitīvu skaitli: ")
+
+try:
+    sk=int(num)
+    if sk>0:
+        for i in range(len(num)-1, -1, -1):
+            print(num[i], end="") #end liek izvadīt skaitļus vienā rindā
+    else:
+        print("Nav ievadīts pozitīvs skaitlis!")
+except ValueError:
+    print("Nav ievadīts skaitlis!")
--- uzd_03/uzd_03_2.py
+++ uzd_03/uzd_03_2.py
@@ -1,2 +1,11 @@
 # Atrast cik skaitļi norādītajā intervālā dalās ar lietotāja norādīto dalītāju.
 
+sakt=int(input("sākuma skaitlis: "))
+beigt=int(input("beigu skaitlis: "))
+dal=int(input("dalitāja skaitlis: "))
+
+cik=0
+for i in range (sakt, beigt+1):
+    if i%dal==0:
+        cik+=1
+print(f"Robežās starp {sakt} un {beigt} - {cik} skaitļi dalās ar {dal}")
