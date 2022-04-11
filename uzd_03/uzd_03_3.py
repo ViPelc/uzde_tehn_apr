7d04ead2d46aeee9e062ce20a57d206f38043a24
ViPelc <vkozus@gmail.com>
2022-04-11 23:04:56 +0300

    Izlabots_aprekins

--- uzd_03/uzd_03_3.py
+++ uzd_03/uzd_03_3.py
@@ -2,21 +2,23 @@
 # Aprēķināt skaitļu summu lietotāja norādītajā intervālā. Ja skaitlis dalās ar 13 vai dalās ar 4 un ir trīsciparu skaitlis, 
 # tad skaitlis nav jāpievieno summai, 
 # bet, ja skaitlis ir četrciparu skaitlis un dalās ar 7, un dalās ar 1000, tad summas skaitīšana ir jāpārtrauc.
+
+# V2
 num = int(input("Ievadi pirmo skaitli: "))
 num2 = int(input("Ievadi otro skaitli: "))
 summa = 0
-for x in range(num, num2+1, 1): #num2 +1 nozīmē ievadīto ciparu ieskaitot
-    if (x>99) and (x%4==0) or (x%13==0) and (x>99):
+for x in range(num, num2+1): 
+    if (len(str(x))==3) and (x%4==0) or (x%13==0) and (len(str(x))==3):
         pass
-    elif(x>999) and (x%7==0) and (x%1000==0): 
-        break #neizpildās, nezinu kāpēc :(
+    elif(len(str(x))==4) and (x%7==0) and (x%1000==0): 
+        break 
     else:
-        summa+=x #'+=x ir tas pats ja rakstītu summa=summa+x'
+        summa+=x
 print(summa)
 
 
 """
-V2
+# V1
 nr = int(input("Ievadi pirmo skaitli: "))
 nr2 = int(input("Ievadi otro skaitli: "))
 summa = 0
